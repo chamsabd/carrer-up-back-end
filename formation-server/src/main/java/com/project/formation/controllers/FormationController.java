ca419f4e825434bbc43a5658bf40a8dc6a602a8e
nourhaine1 <ayadinourhaine@gmail.com>
2022-12-04 12:52:45 +0100

    get formations by category

--- formation-server/src/main/java/com/project/formation/controllers/FormationController.java
+++ formation-server/src/main/java/com/project/formation/controllers/FormationController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -137,4 +138,10 @@ public class FormationController {
 		
 	}
 	*/
+	@RequestMapping("/category/{category}")
+	public List <Formation> findByCategory(@PathVariable String category,Pageable pageable){
+		return frepo.findByCategory(category, pageable);
+	}
+	
+	
 }
--- formation-server/src/main/java/com/project/formation/repositories/FormationRepository.java
+++ formation-server/src/main/java/com/project/formation/repositories/FormationRepository.java
@@ -6,14 +6,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.project.formation.models.Formation;
 
 public interface FormationRepository extends MongoRepository<Formation, Long> {
 	
 	Page<Formation> findAll(Pageable paging);
+	//List <String> findAllCategories();
 	
-	 List<Formation> findByNom(@Param("nom") String nom, @Param("nom") String sort,Pageable pageable);
+	List<Formation> findByCategory(@Param("category") String category,Pageable pageable);
+	 List<Formation> findByNom(@Param("nom") String nom, String sort,Pageable pageable);
+	 
 	void save(Optional<Formation> form);
 }
 
