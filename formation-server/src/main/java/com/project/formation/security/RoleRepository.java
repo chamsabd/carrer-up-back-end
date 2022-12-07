6b6efc22cb7343956418efd0083f2fce1da7c475
chamsabd <chamsabdelwahed42@gmail.com>
2022-12-07 19:59:05 +0100

    add dependency

---
+++ formation-server/src/main/java/com/project/formation/security/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.project.formation.security;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+
+
+public interface RoleRepository extends MongoRepository<Role,String> {
+	  Optional<Role> findByName(ERole name);
+	}
