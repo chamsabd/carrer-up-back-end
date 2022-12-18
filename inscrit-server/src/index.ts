688bd83ca007f331175b4837f1c7caf373dcd644
safedhaouadi <safadhaouadi319@gmail.com>
2022-12-18 13:57:03 +0100

    add inscrit-server to eureka

--- inscrit-server/src/index.ts
+++ inscrit-server/src/index.ts
@@ -1,22 +1,39 @@
 import express,{Request,Response} from "express";
 //1)
 import mongoose from "mongoose";
-import Inscrit from "./inscrit.model";
+import nodemailer from 'nodemailer'
+
+
 //8)
 import bodyParser from "body-parser";
+import axios from 'axios';
+
+import Inscrit from "./inscrit.model";
+
+const mailconfig = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: "isetb48@gmail.com",
+        pass: "isetiset123"
+    },
+    secure: true
+})
+const sb_host = "http://127.0.0.1:8085"
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-//const eurekaHelper = require('./Inscrit');
+const eurekaHelper = require('./eurekaHelper');
 
 app.listen(PORT, () => {
   console.log("inscrit-service on 3000");
 })
 
-//eurekaHelper.registerWithEureka('Inscrit-service', PORT);
+eurekaHelper.registerWithEureka('Inscrit-service', PORT);
 //7)
 app.use(bodyParser.json())
 //4)
-const uri="mongodb://localhost:27017/carrer_up";
+const uri="mongodb://localhost:27017/carrer_up2";
 mongoose.connect(uri,(err)=>{
 if(err)console.log(err)
 else console.log("Mongo DataBase Connected successfully")
@@ -36,7 +53,7 @@ if (err) resp.status(500).send(err);
 else resp.send(inscrit);
 });
 });    
-
+/*
 app.put("/inscrit/:id",(req:Request,resp:Response)=>{
     Inscrit.findByIdAndUpdate(req.params.id,req.body,(err:any)=>{
         if(err) resp.status(500).send(err);
@@ -44,23 +61,58 @@ app.put("/inscrit/:id",(req:Request,resp:Response)=>{
     })
 
 });
-
+*/
 //6)
 app.post("/inscrit",(req:Request,resp:Response)=>{
     let inscrit = new Inscrit(req.body)
     inscrit.save(err=>{ 
         if(err) resp.status(500).send(err);
-        else resp.send(inscrit)
+        else {
+            console.log(inscrit?.toObject().idSession);
+            resp.send(inscrit)
+        }
     });
 });
 
+app.put("/inscrit/:id/accept", (req: Request, resp: Response)=>{
+    // Send put request here ! tw hajtk données eli f lbase tw !!? hahaha!! mlaa bech wohh
+    Inscrit.findOne({"_id": req.params.id}, (err: any, inscrit: any)=>{
+        if (err) return resp.send(err)
+        let idSession = inscrit?.toObject().idSession
+        console.log(idSession)
+        // heya ta3ml fel formation fonction ta3ml -1 l nbrPlace par id Session
+        axios.get(`${sb_host}/SESSION-SERVER/${idSession}/sessions`)
+            .then((session)=>{
+                let idFormation = session.data.Sessions[0].idFormation
+                axios.get(`${sb_host}/formations/${idFormation}`)
+                    .then((formation)=>{
+                        //console.log(formation.data)
+                        resp.send(formation)
+                    })
+                    .catch(err=>{
+                        console.log("err")
+                        console.log(err)
+                    })
+            })
+    })
+})
+app.put("/inscrit/:id/refuse", (req: Request, resp: Response)=>{
+    Inscrit.findByIdAndUpdate(req.params.id, {"etat": "refuser"}, (err)=>{
+        if(err) resp.status(500).send(err);
+        else {
+            // Send mail
+
+            resp.send({message: "updated!"})
+        }
+    })
+})
 app.delete("/inscrit/:id",(req:Request,resp:Response)=>{
     Inscrit.findByIdAndDelete(req.params.id,(err:any,Inscrit:any)=>{
     if (err) resp.status(500).send(err);
         else resp.send("inscrit deleted");
     });
 });
-
+/*
 app.get('/inscritParPage',(req:Request,res:Response)=>{
     const page:number = parseInt(req.query.page?.toString()||'1');
     const size:number = parseInt(req.query.size?.toString()||'5');
@@ -69,7 +121,7 @@ app.get('/inscritParPage',(req:Request,res:Response)=>{
         else res.send(inscrit);
     });
 })
-
+*/
 app.get("/",(req,resp)=>{
     resp.send("hello express !")
 });
--- inscrit-server/src/inscrit.model.ts
+++ inscrit-server/src/inscrit.model.ts
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate";
 //2)
 let inscritSchema=new mongoose.Schema({
-    etat:{type: String,enum: ['accepter', 'refuser','en cours de traitement'],required:true,default:'en cours de traitement'},
+    etat:{
+        type: String,
+        enum: ['accepter', 'refuser','en cours de traitement'],
+        required:true,
+        default:'en cours de traitement'
+    },
     date:{type:Date,required:true,default:new Date()},
     idSession:{type:Number,required:true},
     idUser:{type:Number,required:true},
